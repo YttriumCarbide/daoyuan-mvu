b9c96945740390ad97110c81cfb7d35720dba8c7
watnab1 <mayuchiq@gmail.com>
2026-07-14 18:14:38 +0800

    refactor: update mock status data to reflect current interface requirements

--- src/mock.js
+++ src/mock.js
@@ -1,20 +1,20 @@
 // Mock SillyTavern Globals for Vite Dev Environment
-window.waitGlobalInitialized = async function(name) {
+window.waitGlobalInitialized = async function (name) {
   console.log(`[Mock] waitGlobalInitialized called for ${name}`);
   return Promise.resolve();
 };
 
 const events = {};
-window.eventOn = function(name, callback) {
+window.eventOn = function (name, callback) {
   if (!events[name]) events[name] = [];
   events[name].push(callback);
 };
-window.eventEmit = function(name, data) {
+window.eventEmit = function (name, data) {
   if (events[name]) events[name].forEach(cb => cb(data));
 };
 
-window.errorCatched = function(fn) {
-  return function(...args) {
+window.errorCatched = function (fn) {
+  return function (...args) {
     try {
       return fn(...args);
     } catch (e) {
@@ -56,7 +56,22 @@ const mockMvuData = {
     "道侣": {
       "柳如烟": { "亲密": 100, "境界": "金丹期", "种族": "人族", "desc": "青梅竹马" }
     },
-    "玉简": {},
+    "绝色榜": {
+      "瑶汐": {
+        "排名": 1,
+        "头衔": "九天之主",
+        "仙姿": "容颜绝世，不可方物，自带九天玄气",
+        "群芳谱": "万界美人榜榜首，传闻其一眸可令星辰黯淡",
+        "性别": "女"
+      },
+      "林雪": {
+        "排名": 2,
+        "头衔": "月宫之主",
+        "仙姿": "清冷如霜，孤高清绝",
+        "群芳谱": "世间难得的冰山美人，只可远观",
+        "性别": "女"
+      }
+    },
     "人物": {
       "李长老": { "境界": "元婴期", "门派": "青云宗", "desc": "严厉的长老" }
     },
@@ -66,15 +81,15 @@ const mockMvuData = {
   }
 };
 
-window.getAllVariables = function() {
+window.getAllVariables = function () {
   return mockMvuData;
 };
 
-window.getLastMessageId = function() {
+window.getLastMessageId = function () {
   return 42;
 };
 
-window.getChatMessages = function() {
+window.getChatMessages = function () {
   return [{ message_id: 42, name: "system", mes: "这是一条测试玉简消息" }];
 };
 
@@ -83,14 +98,14 @@ window.Mvu = {
   events: {
     VARIABLE_UPDATE_ENDED: 'VARIABLE_UPDATE_ENDED'
   },
-  getMvuData: function(options) {
+  getMvuData: function (options) {
     console.log('[Mock] Mvu.getMvuData', options);
     return JSON.parse(JSON.stringify(mockMvuData)); // 深度克隆，避免直接修改引用
   },
-  replaceMvuData: async function(fullData, options) {
+  replaceMvuData: async function (fullData, options) {
     console.log('[Mock] Mvu.replaceMvuData', fullData, options);
     Object.assign(mockMvuData, fullData);
-    
+
     // 模拟数据保存后的 UI 更新触发
     setTimeout(() => {
       window.eventEmit(window.Mvu.events.VARIABLE_UPDATE_ENDED);
