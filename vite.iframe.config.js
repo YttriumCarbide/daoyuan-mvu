98b01b550d6c3546e13fa0d9b8c3330900953365
watnab1 <mayuchiq@gmail.com>
2026-07-14 18:45:01 +0800

    feat: add iframe testing environment with dedicated vite configuration and dev script

---
+++ vite.iframe.config.js
@@ -0,0 +1,59 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
+// 注入 Mvu mock 数据与全局依赖的插件
+const mvuMockPlugin = () => {
+  return {
+    name: 'mvu-mock-plugin',
+    apply: 'serve', // 仅在 dev 模式使用 (vite dev)
+    transformIndexHtml(html) {
+      return [
+        {
+          tag: 'script',
+          attrs: { src: 'https://code.jquery.com/jquery-3.6.0.min.js' },
+          injectTo: 'head-prepend'
+        },
+        {
+          tag: 'script',
+          attrs: { src: 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js' },
+          injectTo: 'head-prepend'
+        },
+        {
+          tag: 'script',
+          attrs: { src: '/mock.js' },
+          injectTo: 'head'
+        }
+      ];
+    }
+  }
+};
+
+export default defineConfig({
+  root: 'src',
+  plugins: [
+    vue(),
+    viteSingleFile(),
+    mvuMockPlugin()
+  ],
+  server: {
+    open: '/iframe-test.html'
+  },
+  build: {
+    minify: false,
+    outDir: '../dist',
+    emptyOutDir: true,
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+        iframe: 'src/iframe-test.html'
+      },
+      output: {
+        inlineDynamicImports: true
+      }
+    }
+  }
+});
